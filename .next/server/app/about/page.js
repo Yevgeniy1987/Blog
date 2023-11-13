cd3c2085644d85086cea09e257087b614e843f26
Eugene <evgeniy.lyashko.87@gmail.com>
2023-11-13 17:46:06 -0600

    deleted .next

--- .next/server/app/about/page.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/about/page.js";
-const runtime = require("../../chunks/rsc/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/rsc/node_modules_95ca0c._.js");
-runtime.loadChunk("server/chunks/rsc/[root of the server]__d803cd._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js/(COMPONENT_0)/[project]/src/app/about/page.tsx (ecmascript, Next.js server component)/(COMPONENT_1)/[project]/src/app/error.tsx (ecmascript, Next.js server component)/(COMPONENT_2)/[project]/src/app/layout.tsx (ecmascript, Next.js server component)/(COMPONENT_3)/[project]/src/app/not-found.tsx (ecmascript, Next.js server component) (ecmascript, rsc)", CHUNK_PUBLIC_PATH).exports;
